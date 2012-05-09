057b91ab00e90f49385fb4119073c5e978c60013
Roberto Guerra <uris77@gmail.com>
2012-05-08 23:08:17 -0600

    Implement Steps for Store

--- test/functional/steps/Store_steps.groovy
+++ test/functional/steps/Store_steps.groovy
@@ -1,15 +1,21 @@
 import cucumber.runtime.PendingException
+import static org.junit.Assert.*
+import org.wherecaniget.*
 
 this.metaClass.mixin(cucumber.runtime.groovy.EN)
 
+StoreVM vm
+
 Given(~"^I open new store form\$") {->
-   throw new PendingException()
 }
 
 Given(~"^I add \"([^\"]*)\"\$"){String arg1->
-   throw new PendingException()
+   vm = new StoreVM()
+   vm.store = [name: 'Test Store']
+   vm.storeService = new StoreService()
+   vm.save()
 }
 
 Then(~"^I see \"([^\"]*)\" details\$")  {String arg1->
-   throw new PendingException()
+   assertNotNull vm.store.id
 }
