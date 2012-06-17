f92c4770f8ae20b5a09e9dc223bfa6611539fd38
Roberto Guerra <uris77@gmail.com>
2012-06-17 11:37:20 -0600

    Default filter for stores set to *
    The default filter when searching for stores is set to *. This will list
    all stores.

--- grails-app/vms/org/wherecaniget/StoreListVM.groovy
+++ grails-app/vms/org/wherecaniget/StoreListVM.groovy
@@ -4,14 +4,14 @@ import org.zkoss.bind.annotation.Command
 import org.zkoss.bind.annotation.NotifyChange
 
 class StoreListVM {
-   def filter
+   def filter = "*"
    def storeList = [] 
 
    @NotifyChange(['storeList'])
    @Command
    def search(){
       if(filter.equals("*")){
-         storeList = Store.list()
+         storeList = Store.list([sort:'name'])
       }else{
          storeList = Store.findAllByName(filter)
       }
